7f9de3f025cc7ca4108904e84af28da84b3c5fa9
Caleb Gardner <calebq23@gmail.com>
2024-06-12 05:18:03 -0500

    Added json and bson tags to ApiKey

--- internal/backend/key.go
+++ internal/backend/key.go
@@ -1,10 +1,10 @@
 package backend
 
 type ApiKey struct {
-	Perm  map[string]bool
-	ID    string
-	AppID string
-	Death int64
+	Perm  map[string]bool `json:"perm" bson:"perm"`
+	ID    string          `json:"id" bson:"_id" valkey:",key"`
+	AppID string          `json:"appID" bson:"appID"`
+	Death int64           `json:"death" bson:"death"`
 }
 
 func (k ApiKey) GetID() string {

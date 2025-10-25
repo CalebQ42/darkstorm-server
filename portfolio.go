2a16aca6aef804fe18e9c5ec09056e083cfac02e
Caleb J. Gardner <calebq23@gmail.com>
2025-10-25 09:43:36 -0500

    Fixed CORS for portfolio page

--- portfolio.go
+++ portfolio.go
@@ -6,6 +6,12 @@ import (
 )
 
 func portfolioRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "https://darkstorm.tech")
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "*, Authorization")
+	}
 	selectedTech := r.URL.Query().Get("tech")
 	proj, err := blogApp.Projects(r.Context(), selectedTech)
 	if err != nil {
